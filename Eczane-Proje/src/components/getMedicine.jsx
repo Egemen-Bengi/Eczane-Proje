925d73ecffd69842b04c3faa17289d57c5fdca23
Bengi <114860608+Egemen-Bengi@users.noreply.github.com>
2025-05-02 16:29:41 +0300

    GET medicines

---
+++ Eczane-Proje/src/components/getMedicine.jsx
@@ -0,0 +1,42 @@
+import React, {useEffect, useState} from "react";
+
+const API_URL = "https://v1.nocodeapi.com/bengi/google_sheets/CNbzVtWjswSphVic";
+
+const Get = async () => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    var requestOptions = {
+        method: "get",
+        headers: myHeaders,
+        redirect: "follow",
+        
+    };
+
+    return fetch("https://v1.nocodeapi.com/bengi/google_sheets/CNbzVtWjswSphVic?tabId=Sayfa1", requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            return result
+        })
+        .catch(error => {
+            throw error;
+        });
+}
+
+const GetMedicine = () => {
+    const [medicines, setMedicines] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            try{
+                const response = await Get();
+                setMedicines(response.data || []);
+            } catch (error){
+                console.log(error)
+            }
+        }
+        fetchData();
+    }, []);
+
+    // return <BengiEczane medicines={medicines} />;
+};
+
+export default GetMedicine;
